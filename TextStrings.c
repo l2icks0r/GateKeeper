b9f42b083fd66582b255d985bdd87c8d3902a93a
l2icks0r <ricksaenz1@gmail.com>
2015-10-03 05:18:55 -0700

    Formatting

--- TextStrings.c
+++ TextStrings.c
@@ -6,17 +6,17 @@
 
 void InitTextStrings( void )
 {
-	DecryptString( Splash_Text_0,	 splash_text_0 );	 		// ** RRP BMX GATES **
-	DecryptString( Splash_Text_1,	 splash_text_1 );	 		// Epicenter v0.9.4
+	DecryptString( Splash_Text_0,	 	splash_text_0 );	 		// ** RRP BMX GATES **
+	DecryptString( Splash_Text_1,	 	splash_text_1 );	 		// Epicenter v0.9.4
 
-	DecryptString( Copyright_Text,	 copyright_text );	 		// Copyright 2015
-	DecryptString( l2icks0r_Designs, l2icks0r_designs ); 		// l2icks0r! Designs
+	DecryptString( Copyright_Text,	 	copyright_text );	 		// Copyright 2015
+	DecryptString( l2icks0r_Designs, 	l2icks0r_designs ); 		// l2icks0r! Designs
 
-	DecryptString( Unauthorized_Device, unauthorized_device );	// Unauthorized Device
-	DecryptString( Illegal_Counterfeit, illegal_counterfeit );	// Illegal counterfeit
+	DecryptString( Unauthorized_Device, unauthorized_device );		// Unauthorized Device
+	DecryptString( Illegal_Counterfeit, illegal_counterfeit );		// Illegal counterfeit
 
-	DecryptString( Counterfeit_Box, counterfeit_box );			// Counterfeit Box
-	DecryptString( You_Suck, you_suck );						// You SUCK!!!
+	DecryptString( Counterfeit_Box, 	counterfeit_box );			// Counterfeit Box
+	DecryptString( You_Suck, 			you_suck );					// You SUCK!!!
 }
 
 void DecryptString( char *pDeCoded, const short *pCoded )
