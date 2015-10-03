478af75988d0fd80dc70a7c3c3aa19a12187e4dc
l2icks0r <ricksaenz1@gmail.com>
2015-10-03 16:47:24 -0700

    Added formal text

--- TextStrings.c
+++ TextStrings.c
@@ -6,29 +6,31 @@
 
 void InitTextStrings( void )
 {
-	DecryptString( Splash_Text_0,	 	splash_text_0 		); 	// ** RRP BMX GATES **
-	DecryptString( Splash_Text_1,	 	splash_text_1 		); 	// Epicenter v0.9.4
+	DecryptString( Splash_Text_0,	 		splash_text_0 			); 	// ** RRP BMX GATES **
+	DecryptString( Splash_Text_1,	 		splash_text_1 			); 	// Epicenter v0.9.4
 
-	DecryptString( Copyright_Text,	 	copyright_text 		);	// Copyright 2015
-	DecryptString( l2icks0r_Designs, 	l2icks0r_designs 	); 	// l2icks0r! Designs
+	DecryptString( Device_Information,		device_information 		);	// -Device Information-
+	DecryptString( Ownership_Information, 	ownership_information 	); 	// ...
 
-	DecryptString( Unauthorized_Device, unauthorized_device );	// Unauthorized Device
-	DecryptString( Illegal_Counterfeit, illegal_counterfeit );	// Illegal counterfeit
+	DecryptString( Design_By,				design_by 				);	// Epicenter design by:
+	DecryptString( Designer_Information, 	designer_information 	); 	// Richard E. Saenz Jr.
 
-	DecryptString( Counterfeit_Box, 	counterfeit_box 	);	// Counterfeit Box
-	DecryptString( You_Suck, 			you_suck 			);	// You SUCK!!!
+	DecryptString( Unauthorized_Device, 	unauthorized_device 	);	// Unauthorized Device
+	DecryptString( Illegal_Counterfeit, 	illegal_counterfeit 	);	// Illegal counterfeit
 }
 
 void DecryptString( char *pDeCoded, const short *pCoded )
 {
-	int i	  = 0;
 	int mask  = 0;
 	int angle = *pCoded++;
 	int theta = *pCoded++;
 
-	memset( pDeCoded, 0, 20 );
+	int coded_length = 0;
+	for( ; *(pCoded + 2 + coded_length) != 0; coded_length++ );
 
-	for( i = 0; i < 20 && *pCoded != 0; i++, pCoded++, pDeCoded++ )
+	memset( pDeCoded, 0, coded_length + 1 );
+
+	for( ; *pCoded != 0; pCoded++, pDeCoded++ )
 	{
 		angle += theta;
 
